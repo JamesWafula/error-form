7f4d60a7e9b381100ba8ece070bc4a3d5fd26d32
JomoloWaf <wafula.james@live.com>
2022-02-09 06:33:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <header-section />
+    <form-section />
+  </v-container>
+</template>
+
+<script>
+import headerSection from "@/components/headerSection.vue";
+import formSection from "@/components/formSection.vue";
+export default {
+  name: "Home",
+
+  components: {
+    headerSection,
+    formSection,
+  },
+};
+</script>
